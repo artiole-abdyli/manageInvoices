dcf884d7adfbfd30293ceeacec60534faab2b15e
artiole <artiole.abdyli@mesh-com.at>
2025-05-11 14:15:55 +0200

    creation modal for reservation

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -1,7 +1,17 @@
 "use client";
-import { Button, DatePicker, Form, Input, Modal, Table } from "antd";
+import {
+  Button,
+  DatePicker,
+  Form,
+  Input,
+  Modal,
+  Table,
+  InputNumber,
+  Row,
+  Select,
+  Col,
+} from "antd";
 import { useState } from "react";
-
 export default function ReservationsPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const columns = [
@@ -52,7 +62,12 @@ export default function ReservationsPage() {
         </Button>
       </div>
       <Table columns={columns}></Table>
-      <Modal open={isModalOpen} onCancel={handleClose}>
+      <Modal
+        open={isModalOpen}
+        onCancel={handleClose}
+        width={700}
+        title="Create new reservation"
+      >
         <Form style={{ paddingTop: "30px", paddingBottom: "30px" }}>
           <Form.Item name="date" label="Date of reservation">
             <DatePicker />
@@ -60,6 +75,52 @@ export default function ReservationsPage() {
           <Form.Item name="returning_date" label="Returning date">
             <DatePicker />
           </Form.Item>
+          <Form.Item name="product_id" label="Product">
+            <Select placeholder="Select a product">
+              <Select.Option value={1}>Wedding Dress</Select.Option>
+              <Select.Option value={2}>Evening Gown</Select.Option>
+              <Select.Option value={3}>Bridesmaid Dress</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item name="contact_id" label="Contact">
+            <Select placeholder="Select a contact">
+              <Select.Option value={1}>Anna Smith</Select.Option>
+              <Select.Option value={2}>John Doe</Select.Option>
+              <Select.Option value={3}>Elira Morina</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Row gutter={16}>
+            <Col span={8}>
+              <Form.Item name="price" label="Total price">
+                <InputNumber
+                  name="price"
+                  style={{ width: "100%" }}
+                  placeholder="€"
+                />
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item name="deposit" label="Deposit">
+                <InputNumber
+                  name="deposit"
+                  style={{ width: "100%" }}
+                  placeholder="€"
+                />
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item name="remaining_payment" label="Remaining payment">
+                <InputNumber
+                  name="remaining_payment"
+                  style={{ width: "100%" }}
+                  placeholder="€"
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+
           <Form.Item name="extra_requirement" label="Extra requirement">
             <Input.TextArea
               rows={4}
