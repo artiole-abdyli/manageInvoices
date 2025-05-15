c265ed611395a9ecb53ec30a506a293cfeeeafe4
artiole <artiole.abdyli@mesh-com.at>
2025-05-15 21:59:26 +0200

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from "next";
-import "./globals.css";
+"use client";
 
+import { usePathname } from "next/navigation";
 import { AuthProvider } from "./context/AppContext";
 import Sidebar from "@/src/components/sidebar/Sidebar";
-
-export const metadata: Metadata = {
-  title: "Chique dolls exclusive",
-};
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
+  const isAuthPage = ["/login", "/register"].includes(pathname);
+
   return (
     <html lang="en">
       <body className="flex h-screen overflow-hidden">
-        <Sidebar children={children} />
+        <AuthProvider>
+          {isAuthPage ? children : <Sidebar>{children}</Sidebar>}
+        </AuthProvider>
       </body>
     </html>
   );
